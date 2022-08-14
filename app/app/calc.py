ca541ad4078f30c1bd829112c96cb574e0b9de1e
trumkhungbo <trumkhungbo234@gmail.com>
2022-08-14 10:14:07 +0700

    add psycopg2

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator funtions
+"""
+
+def add(x,y):
+    """
+    Add x and y and return x+y"""
+    return x + y
+
+def subtract(x,y):
+    """Subtract x from y and return x-y"""
+    return x - y
